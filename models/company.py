82b8d08e06f1adde3b9ddc9b904af3b86e0a9581
Hamsini <hamsinimoni.com>
2026-06-27 16:23:43 +0530

    db

--- models/company.py
+++ models/company.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column,Integer,String,Enum,relationship
-from database import Base,engine,Sessionlocal
+from database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
-class CompanyBase(Base):
-    __tablename__="companies"
-    id=Column(Integer,primary_key=True,index=True)
-    name=Column(String,nullable=False,index=True)
-    email=Column(String,unique=True)
-    phone=Column(String,unique=True)
-    jobs=relationship("Job",back_populates="company")
+class Company(Base):
+    __tablename__ = "companies"
 
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    email = Column(String, unique=True)
+    phone = Column(String, unique=True)
+
+    jobs = relationship("Job", back_populates="company")
--- models/job.py
+++ models/job.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Column,Integer,String,Enum,ForeignKey,relationship
-from models.company import Company
-from database import Base,engine,Sessionlocal
+from database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Job(Base):
-    __tablename__="jobs"
-    id=Column(Integer,primary_key=True,index=True)
-    title=Column(String,nullable=False)
-    description=Column(String)
-    salary=Column(Integer)
-    company_id=Column(Integer,ForeignKey("companies.id"))
+    __tablename__ = "jobs"
 
-    company=relationship("Company",back_populates="jobs")
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    description = Column(String)
+    salary = Column(Integer)
 
+    company_id = Column(Integer, ForeignKey("companies.id"))
+
+    company = relationship("Company", back_populates="jobs")
